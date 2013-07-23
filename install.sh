1d53acedbe45e627b6a5c29844fbe976c72b73e5
Scott Kidder <kidder.scott@gmail.com>
2013-07-23 14:35:29 +0000

    Added gitignore file.  Added install.sh script to install and configure host

---
+++ install.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+#
+# File: install.sh
+# Author: Scott Kidder
+# Purpose: This script will configure a newly-imaged Raspberry Pi running 
+#   Debian Wheezy 2013-05-25 with the dependencies and HSMM-Pi components.
+#
+
+PROJECT_HOME=${HOME}/hsmm-pi
+
+cd ${HOME}
+
+# Update list of packages
+sudo apt-get update
+
+# Install Web Server deps
+sudo apt-get install apache2
+sudo apt-get install php5
+sudo apt-get install sqlite
+sudo apt-get install php-pear
+
+# TODO
+# Verify this package is correct
+sudo apt-get install php5-sqlite 
+sudo apt-get install olsrd
+
+# TODO
+# install cakephp with Pear
+
+# Checkout the HSMM-Pi project
+git clone https://github.com/urlgrey/hsmm-pi.git
+
+# Set symlink to webapp
+cd /var/www
+if [ ! -e /var/www/hsmm-pi ]; then
+    sudo ln -s ${PROJECT_HOME}/src/var/www/hsmm-pi
+fi
+
+# Create temporary directory used by HSMM-PI webapp, granting write priv's to www-data
+cd ${PROJECT_HOME}/src/var/www/hsmm-pi
+mkdir -p tmp/cache/models
+mkdir -p tmp/cache/persistent
+mkdir -p tmp/logs
+mkdir -p tmp/persistent
+sudo chown -R pi.www-data tmp
+sudo chmod -R 775 tmp
+
+# Set permissions on system files to give www-data group write priv's
+for file in /etc/hosts /etc/hostname /etc/resolv.conf /etc/network/interfaces /etc/rc.local; do
+    sudo chgrp www-data ${file}
+    sudo chmod g+w ${file}
+done
+    
+sudo chgrp -R www-data /etc/olsrd
+sudo chmod g+w -R /etc/olsrd
+
+sudo chgrp www-data /etc/dnsmasq.d
+sudo chmod 775 /etc/dnsmasq.d
+
+# Copy scripts into place
+if [ ! -e /usr/local/bin/callsign_announcement.sh ]; then
+    sudo cp ${PROJECT_HOME}/src/var/www/hsmm-pi/webroot/files/callsign_announcement.sh.template /usr/local/bin/callsign_announcement.sh
+    sudo chgrp www-data /usr/local/bin/callsign_announcement.sh
+    sudo chmod 775 /usr/local/bin/callsign_announcement.sh
+fi
+
+# TODO add CRON job for reboot
+
+# TODO add CRON job for callsign announement
