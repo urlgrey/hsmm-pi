01659b228312f952b472e91e955d432c877b5699
Scott Kidder <kidder.scott@gmail.com>
2013-08-06 03:06:55 +0000

    Added lock file for script that read GPS coordinates

--- src/usr/local/bin/read_gps_coordinates.pl
+++ src/usr/local/bin/read_gps_coordinates.pl
@@ -6,10 +6,12 @@ use Net::GPSD3;
 my $host=shift || undef;
 my $port=shift || undef;
 
+touch '/var/run/read_gps_coordinates.lock';
 my $gpsd=Net::GPSD3->new(host=>$host, port=>$port); #default host port as undef
 
 $gpsd->addHandler(\&tpv);
 $gpsd->watch;
+unlink '/var/run/read_gps_coordinates.lock' or warn "Could not unlink $file: $!";
 
 sub tpv {
     my $tpv=shift;
@@ -17,5 +19,6 @@ sub tpv {
     open(COORDS, '>/var/run/latlong-input-olsrd') or die "Cannot open file for write";
     printf COORDS "%s,%s", $tpv->lat, $tpv->lon;
     close(COORDS);
+    unlink '/var/run/read_gps_coordinates.lock' or warn "Could not unlink $file: $!";
     exit 0;
 }
