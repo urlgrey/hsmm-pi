68c862219e8a8449cb005a1aad45d86964e6943f
Scott Kidder <kidder.scott@gmail.com>
2013-08-08 14:15:28 +0000

    Modified the Location lat-lon form validation

--- src/var/www/hsmm-pi/Model/LocationSetting.php
+++ src/var/www/hsmm-pi/Model/LocationSetting.php
@@ -3,8 +3,8 @@ class LocationSetting extends AppModel {
 
   public $validate = array(
 			   'location_source' => array('required' => array('rule' => '/^(fixed|gps)$/i', 'message' => 'The location source must be set to GPS or a fixed location.')),
-			   'lat' => array('required' => array('rule' => '/\d+\.?\d*/', 'message' => 'Latitude must be a decimal value')),
-			   'lon' => array('required' => array('rule' => '/\d+\.?\d*/', 'message' => 'Longitude must be a decimal value')),
+			   'lat' => array('rule' => array('decimal')),
+			   'lon' => array('rule' => array('decimal')),
 			   );
 }
 ?>
