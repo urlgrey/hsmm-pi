a1b2819d45d903eb27404f3bb95463554a970980
Scott Kidder <kidder.scott@gmail.com>
2013-08-09 01:19:30 +0000

    Updated model validation rules

--- src/var/www/hsmm-pi/Model/LocationSetting.php
+++ src/var/www/hsmm-pi/Model/LocationSetting.php
@@ -2,6 +2,7 @@
 class LocationSetting extends AppModel {
 
   public $validate = array(
+			   'maps_api_key' => array('rule' => array('maxlength', 256), 'message' => 'The maps API key length must not exceed 256 characters.'),
 			   'location_source' => array('required' => array('rule' => '/^(fixed|gps)$/i', 'message' => 'The location source must be set to GPS or a fixed location.')),
 			   'lat' => array('rule' => array('decimal')),
 			   'lon' => array('rule' => array('decimal')),
