69958a7af430f26762e4e1c7db7b80b7639308ae
Scott Kidder <kidder.scott@gmail.com>
2013-08-09 04:17:33 +0000

    Modified the script that reads from GPSD to not write to the text file read by OLSRD if it is unable to get the coordinates.  I have observed that the OLSRD nameservice plugin goes crazy if the text file with latitude-longtitude is empty or malformed.

--- src/usr/local/bin/read_gps_coordinates.pl
+++ src/usr/local/bin/read_gps_coordinates.pl
@@ -18,6 +18,12 @@ unlink '/var/run/read_gps_coordinates.lock' or warn "Could not unlink $lock_file
 sub tpv {
     my $tpv=shift;
     return unless $tpv->class eq "TPV";
+
+    # have observed cases where the tpv structure is missing these fields, bail
+    if ((0 == length($tpv->lat // '')) || (0 == length($tpv->lon // ''))) {
+        exit -1;
+    }
+
     open(COORDS, '>/var/run/latlong-input-olsrd') or die "Cannot open file for write";
     printf COORDS "%s,%s", $tpv->lat, $tpv->lon;
     close(COORDS);
