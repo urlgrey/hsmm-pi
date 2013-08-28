81f36ccda668d3ccb995083945894070452491ed
Scott Kidder <kidder.scott@gmail.com>
2013-08-28 14:25:49 +0000

    Advertise services on the mesh in both WAN and LAN wired modes.  Previously they were advertised in LAN mode only.

--- src/var/www/hsmm-pi/Controller/AppController.php
+++ src/var/www/hsmm-pi/Controller/AppController.php
@@ -188,14 +188,16 @@ LoadPlugin \"olsrd_dyn_gw_plain.so.0.4\"
       } else {
       $olsrd_gateway = "";
       $olsrd_network_services = "";
-      if ($network_services != NULL && sizeof($network_services) > 0) {
-	foreach($network_services as $service) {
-	  $olsrd_network_services .= "
+    }
+
+    if ($network_services != NULL && sizeof($network_services) > 0) {
+      foreach($network_services as $service) {
+	$olsrd_network_services .= "
     PlParam \"service\" \"".$service['NetworkService']['service_protocol_name']."://".$network_setting['NetworkSetting']['node_name'].".local.mesh:".$service['NetworkService']['forwarding_port']."|".$service['NetworkService']['protocol']."|".$service['NetworkService']['name']."\"
 ";
-	}
       }
     }
+    
 
     $transmit_location_option = null;
     if ($location['LocationSetting']['transmit_location_enabled'] == TRUE) {
