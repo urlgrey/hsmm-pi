0b74de21280165e5a19cdb0f9ae11c344e110876
bwattendorf <bwattendorf@gmail.com>
2014-05-27 11:30:41 -0400

    added a cleanup script to remove what is not needed for a bbhn node

---
+++ cleanup-pi.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+
+
+
+apt-get purge --auto-remove -y \
+scratch \
+debian-reference-en dillo idle3 python3-tk idle python-pygame python-tk \
+lightdm gnome-themes-standard gnome-icon-theme raspberrypi-artwork \
+gvfs-backends gvfs-fuse desktop-base lxpolkit netsurf-gtk zenity xdg-utilse \
+mupdf gtk2-engines alsa-utils  lxde lxtask menu-xdg gksu \
+midori xserver-xorg xinit xserver-xorg-video-fbdev \
+libraspberrypi-dev libraspberrypi-doc \
+dbus-x11 libx11-6 libx11-data libx11-xcb1 x11-common x11-utils \
+lxde-icon-theme gconf-service gconf2-common
