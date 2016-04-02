ae2f2b6ed7a77e5a5f2e92e583c8276d732af2ee
Clayton Smith <argilo@gmail.com>
2016-04-01 20:05:25 -0400

    Fix deprecations.

--- src/var/www/hsmm-pi/Model/NetworkService.php
+++ src/var/www/hsmm-pi/Model/NetworkService.php
@@ -2,12 +2,12 @@
 class NetworkService extends AppModel {
 
 	public $validate = array(
-		'name' => array('required' => array('rule' => array('notEmpty'), 'message' => 'Service name is required')),
-		'service_protocol_name' => array('required' => array('rule' => array('notEmpty'), 'message' => 'Service protocol is required')),
+		'name' => array('required' => array('rule' => array('notBlank'), 'message' => 'Service name is required')),
+		'service_protocol_name' => array('required' => array('rule' => array('notBlank'), 'message' => 'Service protocol is required')),
 		'host' => array('required' => array('rule' => '/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/i', 'message' => 'A valid IP address is required')),
-		'port' => array('required' => array('rule' => array('notEmpty'), 'message' => 'Port is required')),
-		'forwarding_port' => array('required' => array('rule' => array('notEmpty'), 'message' => 'Forwarding port is required')),
-		'protocol' => array('required' => array('rule' => array('notEmpty'), 'message' => 'Protocol is required')),
+		'port' => array('required' => array('rule' => array('notBlank'), 'message' => 'Port is required')),
+		'forwarding_port' => array('required' => array('rule' => array('notBlank'), 'message' => 'Forwarding port is required')),
+		'protocol' => array('required' => array('rule' => array('notBlank'), 'message' => 'Protocol is required')),
 	);
 
 }
