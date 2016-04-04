eaef7beedeacd6507ac91ee22dbd386e15254334
Clayton Smith <argilo@gmail.com>
2016-04-03 21:16:49 -0400

    Wait one minute before shutting down or rebooting.

--- src/var/www/hsmm-pi/Controller/SystemController.php
+++ src/var/www/hsmm-pi/Controller/SystemController.php
@@ -4,7 +4,7 @@ class SystemController extends AppController {
 
 	public function reboot() {
 
-		exec('sudo /sbin/shutdown -r now');
+		exec('sudo /sbin/shutdown -r +1');
 
 		$this->Flash->success(__('Reboot initiated, please reload this page in 2 minutes.'));
 		$this->redirect(array('controller' => 'status', 'action' => 'index'));
@@ -12,7 +12,7 @@ class SystemController extends AppController {
 
 	public function shutdown() {
 
-		exec('sudo /sbin/shutdown -h now');
+		exec('sudo /sbin/shutdown -h +1');
 
 		$this->Flash->success(__('Shutdown initiated, goodbye.'));
 		$this->redirect(array('controller' => 'status', 'action' => 'index'));
